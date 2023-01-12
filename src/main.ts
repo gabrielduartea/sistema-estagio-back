b1ddd290120adb31369f6be70ea8e247f4a85984
gabrielduartea <gaaguilar384@gmail.com>
2023-01-11 23:35:37 -0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidateInputPipe } from './core/pipes/validate.pipe';
 
-import cors = require('cors');
-const corsOptions = {
-  origin: 'https://sistema-estagio-back-production.up.railway.app',
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidateInputPipe());
-  app.use(cors(corsOptions));
   await app.listen(5000);
 }
 bootstrap();
