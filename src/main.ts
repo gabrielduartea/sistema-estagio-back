2b2e312243be7acc328bdffd13ba2e846fe424d3
gabrielduartea <gaaguilar384@gmail.com>
2023-01-12 20:27:40 -0300

    aaaa

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidateInputPipe } from './core/pipes/validate.pipe';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidateInputPipe());
   app.use(function (req, res, next) {
