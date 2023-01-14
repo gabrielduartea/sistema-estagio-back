509a36a75771e75306995d0dfbd9debc34d29186
gabrielduartea <gaaguilar384@gmail.com>
2023-01-14 15:39:48 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidateInputPipe());
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    next();
-  });
   app.enableCors();
   await app.listen(3003, () => console.log(`Server running on port ${3003}`));
 }
