ca6b88e233e6a75e76df46bbf65732d8668eca87
gabrielduartea <gaaguilar384@gmail.com>
2023-01-14 20:55:04 -0300

    testando

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidateInputPipe } from './core/pipes/validate.pipe';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: true,
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
