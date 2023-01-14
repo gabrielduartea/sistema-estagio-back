31091f1465f743c7f33e59d4d808d25530196cae
gabrielduartea <gaaguilar384@gmail.com>
2023-01-14 15:43:42 -0300

    q

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,9 @@ import { AppModule } from './app.module';
 import { ValidateInputPipe } from './core/pipes/validate.pipe';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidateInputPipe());
-  app.enableCors();
   await app.listen(3003, () => console.log(`Server running on port ${3003}`));
 }
 bootstrap();
