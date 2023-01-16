60ad8393e0c3fec70417d5d348883a019baca7aa
gabrielduartea <gaaguilar384@gmail.com>
2023-01-16 14:43:06 -0300

    ajuste main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-
+  app.setGlobalPrefix('api/v1');
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
